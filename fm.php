ade5e94c40195bbcdeb17b1374b050fdb2a11c9e
pf <panzerfaust@ev0lve.xyz>
2024-09-26 14:37:10 +0500

    refactored cron, added version to simflex

--- fm.php
+++ fm.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once 'Core/Simflex.php';
+
+$sf = new \App\Core\Simflex();
+const SF_LOCATION = SF_LOCATION_SITE;
+$sf->init();
 
 $mgr = new \Simflex\FileManager\Manager(dirname(__FILE__) . '/uf', function () {
     return !!\Simflex\Core\Container::getUser();
