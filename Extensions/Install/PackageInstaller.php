4455aac31a2b89b20ba6baea0ab4aa141828bb5c
pf <panzerfaust@ev0lve.xyz>
2024-11-08 17:03:55 +0500

    Moved PackageInstaller to core, add missing composer/composer in require-dev

--- Extensions/Install/PackageInstaller.php
+++ Extensions/Install/PackageInstaller.php
@@ -8,40 +8,13 @@ class PackageInstaller
 {
     public static function onPackageEvent(PackageEvent $event): void
     {
-        // get package
-        $operation = $event->getOperation();
-        if (method_exists($operation, 'getPackage')) {
-            $package = $operation->getPackage();
-        } elseif (method_exists($operation, 'getTargetPackage')) {
-            $package = $operation->getTargetPackage();
-        }
-
-        if (!isset($package) || !$package) {
-            return;
-        }
-
-        // check if it's a Simflex package
-        $path = $event->getComposer()->getInstallationManager()->getInstallPath($package);
-        if (!is_dir($path . '/provider/extension')) {
-            return;
-        }
-
-        $root = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
-
-        // copy files
-        static::copyContents($path . '/database/migrations', $root . '/database/migrations');
-        static::copyContents($path . '/database/seeders', $root . '/database/seeders');
-        static::copyContents($path . '/provider/extension', $root . '/provider/extension');
-    }
-
-    protected static function copyContents(string $from, string $to): void
-    {
-        if (!is_dir($from) || !is_dir($to)) {
-            return;
-        }
-
-        foreach (scandir($from) as $file) {
-            copy($from . '/' . $file, $to . '/' . $file);
+        // make sure core is installed, if so - run the actual thing
+        $path = $event->getComposer()->getConfig()->get(
+                'vendor-dir'
+            ) . '/growtask/simflex/Extensions/Install/PackageInstaller.php';
+        if (is_file($path)) {
+            require_once $path;
+            \Simflex\Extensions\Install\PackageInstaller::onPackageEvent($event);
         }
     }
 }
