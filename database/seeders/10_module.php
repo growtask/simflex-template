6c2daac7c010d52c0af9a7f835c4fe200a97d08c
dglushkov <dglushkov@findmykids.org>
2026-06-07 21:20:12 +0500

    Добавлены модули Код и Текстовый блок

--- database/seeders/10_module.php
+++ database/seeders/10_module.php
@@ -1,7 +1,10 @@
 <?php
 
+use Simflex\Admin\Fields\FieldText;
 use Simflex\Core\Models\Module;
 use Simflex\Core\Models\ModuleItem;
+use Simflex\Core\Models\ModuleParam;
+use Simflex\Core\Models\StructField;
 
 return new class implements \Simflex\Core\DB\Seeder {
     public function seed(): void
@@ -34,7 +37,7 @@ return new class implements \Simflex\Core\DB\Seeder {
 
         ModuleItem::bulkInsert([
             [
-                'module_id' => 2,
+                'module_id' => $adminBc->module_id,
                 'menu_id' => null,
                 'posname' => 'breadcrumbs',
                 'active' => 1,
@@ -42,6 +45,48 @@ return new class implements \Simflex\Core\DB\Seeder {
                 'name' => 'Admin - Хлебные крошки',
                 'params' => ''
             ],
+        ]);
+
+        $codeModule = Module::insertStatic([
+            'class' => '\Simflex\Extensions\Code\Code',
+            'name' => 'Код',
+            'type' => 'site',
+        ]);
+        $leftPosition = ModuleParam::insertStatic([
+            'module_id' => $codeModule->module_id,
+            'position' => 'left',
+            'name' => 'module_param_content',
+            'label' => 'Контент',
+            'params' => 'a:1:{s:17:"module_param_main";a:1:{s:13:"default_value";s:0:"";}}',
+        ]);
+        ModuleParam::insertStatic([
+            'module_id' => $codeModule->module_id,
+            'param_pid' => $leftPosition->getId(),
+            'field_id' => StructField::byClass(FieldText::class)->getId(),
+            'name' => 'content',
+            'label' => 'Текст',
+            'params' => 'a:2:{s:17:"module_param_main";a:1:{s:13:"default_value";s:0:"";}s:4:"main";a:2:{s:11:"editor_mini";s:1:"0";s:11:"editor_full";s:1:"0";}}',
+        ]);
+
+        $blockModule = Module::insertStatic([
+            'class' => '\Simflex\Extensions\Block\Block',
+            'name' => 'Текстовый блок',
+            'type' => 'site',
+        ]);
+        $leftPosition = ModuleParam::insertStatic([
+            'module_id' => $blockModule->module_id,
+            'position' => 'left',
+            'name' => 'module_param_content',
+            'label' => 'Контент',
+            'params' => 'a:1:{s:17:"module_param_main";a:1:{s:13:"default_value";s:0:"";}}',
+        ]);
+        ModuleParam::insertStatic([
+            'module_id' => $codeModule->module_id,
+            'param_pid' => $leftPosition->getId(),
+            'field_id' => StructField::byClass(FieldText::class)->getId(),
+            'name' => 'content',
+            'label' => 'Текст',
+            'params' => 'a:2:{s:17:"module_param_main";a:1:{s:13:"default_value";s:0:"";}s:4:"main";a:2:{s:11:"editor_mini";s:1:"0";s:11:"editor_full";s:1:"1";}}',
         ]);
     }
 };
